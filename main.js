71fd34c28e15e0f0ff36762f26e0ab74c94191f8
gabrielluiz01 <gabriel.luizvieira01@gmail.com>
2020-02-13 05:01:31 -0300

    trabalho pronto faltando limpeza de codigo

--- main.js
+++ main.js
@@ -1,30 +1,103 @@
 let imageList = [
-   {
-      url: '',
-      text: '',
-   },
-   {
-      url: '',
-      text: '',
-   },
-   {
-      url: '',
-      text: '',
-   },
+   'assets/orange.png',
+   'assets/mussum.png',
+   'assets/image_rotate.png',
 ]
 
+let counter = 1;
+let image = document.querySelector('.image-slide')
+let text = document.querySelector('.text-slide')
+let title = document.querySelector('.title-slide')
+image.classList.add("image-slide")
+
+
+function setImage(){
+   image.setAttribute('src', imageList[counter])
+
+}
+let slide = document.querySelector('.slide')
 let prev = document.querySelector('.arrow-prev')
 let next = document.querySelector('.arrow-next')
-let text = document.querySelector('.slide-paragraph')
-let image = document.querySelector('.slide')
-let slide = document.querySelector('.slide')
-let counter = 0
 
-function slideNext(){
-   if(counter > 0){
-      counter = counter +1
-      image.setAttribute('', imageList[counter].url)
-      title.innerText = `${imageList[counter].title}`
-      text.innerText = `${imageList[counter].text}`
+
+
+function prevImage(){
+   if(counter > 0 ){
+      counter = counter - 1   
+      
+   }else{
+      counter = 2
+   }
+   setImage()
+}
+
+function nextImage(){
+   if(counter < 2){
+      counter = counter + 1;
+   }else{
+      counter = 0;
+   }
+   setImage();
+}
+
+function ballSlide1(){''
+   if(counter !== 2){
+      counter =  2
+   }else{
+      counter = 1
+   }
+   document.querySelector('.ball1').style.background = '#1FA4E9';
+   document.querySelector('.ball2').style.background = '#9A9A9A';
+   document.querySelector('.ball3').style.background = '#9A9A9A';
+   setImage()
+}
+function ballSlide2(){
+   if(counter !== 1){
+      counter = 1
+   }else{
+      counter = 2
+   }
+   document.querySelector('.ball1').style.background = '#9A9A9A';
+   document.querySelector('.ball2').style.background = '#1FA4E9';
+   document.querySelector('.ball3').style.background = '#9A9A9A';
+   setImage()
+}
+function ballSlide3(){
+   if(counter !== 2){
+      counter = 2
+   }else{
+      counter = 0
+   }
+   document.querySelector('.ball2').style.background = '#9A9A9A';
+   document.querySelector('.ball3').style.background = '#1FA4E9';
+   document.querySelector('.ball1').style.background = '#9A9A9A';
+   setImage()
+}
+
+function init(){
+   slide.appendChild(slide)
+   slide.appendChild(prev);
+   slide.appendChild(image);
+   slide.appendChild(next);   
+}
+init()
+
+
+
+function handleMenu(ev){
+   let menu = document.querySelector('.menu')
+   if(menu.style.display === 'flex'){
+      menu.style.display = 'none'
+      document.querySelector('.x-menu').style.display = 'none'
+
+   }else{
+      menu.style.display = 'flex'
+      document.querySelector('.x-menu').style.display = 'block'
    }
 }
+   
+
+
+
+
+
